0458366e4336542cd2d4066dedefcab1a4b4a2c0
Jason Casareno <jcasareno@csumb.edu>
2022-10-03 18:55:54 -0700

    Update AudioManager.cs
    Audio fades out a bit when the game is over.

--- Project/Assets/JasonC/Scripts/AudioManager.cs
+++ Project/Assets/JasonC/Scripts/AudioManager.cs
@@ -8,11 +8,28 @@ public class AudioManager : MonoBehaviour
     public AudioSource soudtrackAudioSource;
     public float fadeTime = 1f;
 
+    private bool gameEnd = false;
+
     private void Start()
     {
+        FindObjectOfType<GameManager>().changePhase += UpdateCurrentState;
+        
         StartCoroutine(FadeIn(soudtrackAudioSource, fadeTime));
     }
 
+    private void Update()
+    {
+        if (gameEnd)
+        {
+            StartCoroutine(FadeOut(soudtrackAudioSource, fadeTime));
+        }
+    }
+
+    void UpdateCurrentState(GamePhase state)
+    {
+        gameEnd = state == GamePhase.END;
+    }
+    
     public IEnumerator FadeIn(AudioSource audioSource, float FadeTime) {
         audioSource.Play();
         audioSource.volume = 0f;
@@ -20,5 +37,14 @@ public class AudioManager : MonoBehaviour
             audioSource.volume += Time.deltaTime / FadeTime;
             yield return null;
         }
+    }
+    
+    public IEnumerator FadeOut(AudioSource audioSource, float FadeTime) {
+        float startVolume = audioSource.volume;
+        while (audioSource.volume > 0.15) {
+            audioSource.volume -= startVolume * Time.deltaTime / FadeTime;
+            yield return null;
+        }
+        gameEnd = false;
     }
 }
