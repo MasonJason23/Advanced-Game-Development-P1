1589965a8e0a698e09d3de382db456930f236f97
Jason Casareno <jcasareno@csumb.edu>
2022-10-03 19:00:51 -0700

    Audio Modifications
    - Change BGM in scene
    - Modified volume fade out limit

--- Project/Assets/JasonC/Scripts/AudioManager.cs
+++ Project/Assets/JasonC/Scripts/AudioManager.cs
@@ -41,7 +41,7 @@ public class AudioManager : MonoBehaviour
     
     public IEnumerator FadeOut(AudioSource audioSource, float FadeTime) {
         float startVolume = audioSource.volume;
-        while (audioSource.volume > 0.15) {
+        while (audioSource.volume > 0.25) {
             audioSource.volume -= startVolume * Time.deltaTime / FadeTime;
             yield return null;
         }
